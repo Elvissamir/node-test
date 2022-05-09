918c418a18400cc6df91eb813f99874cbfb5a345
Elvissamir <elvissamir18@gmail.com>
2022-05-09 15:01:19 -0400

    Install nodemon package

--- src/controllers/login.controller.js
+++ src/controllers/login.controller.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 
 router.post('/', async (req, res) => {
-    // const { error, token } = await validateLogin(req.body)
+    const { error, token } = await validateLogin(req.body)
 
     if (error) 
         return res.status(400).send('Invalid email or password')
