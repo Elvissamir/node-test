552e04c3d48605608af7618cf4d0667d348be7a5
Elvissamir <elvissamir18@gmail.com>
2022-05-09 15:03:08 -0400

    Reinstall nodemon as dev dependency

--- src/controllers/login.controller.js
+++ src/controllers/login.controller.js
@@ -3,6 +3,7 @@ const router = require('express').Router()
 router.post('/', async (req, res) => {
     const { error, token } = await validateLogin(req.body)
 
+    error = false
     if (error) 
         return res.status(400).send('Invalid email or password')
 
