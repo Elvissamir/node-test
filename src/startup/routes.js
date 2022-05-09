294caa9fc8d0d223955014b5511df82de87cd103
Elvissamir <elvissamir18@gmail.com>
2022-05-09 19:27:30 -0400

    Add error middleware

--- src/startup/routes.js
+++ src/startup/routes.js
@@ -1,8 +1,10 @@
 const { loginEndpoint, userEnpoint } = require('../endpoints/index')
 const loginController = require('../controllers/login.controller')
 const usersController = require('../controllers/users.controller')
+const error = require('../middleware/error')
 
 module.exports = function (app) {
    app.use(loginEndpoint, loginController)
    app.use(userEnpoint, usersController)
+   app.use(error)
 }
